9c90ff61429a0970dd1d840f6d0aace7de114c38
ElanurGuduk <49445642+GudukElanur@users.noreply.github.com>
2024-05-09 15:09:03 +0300

    #6 Update bootcampLİst component

--- src/app/features/components/bootcamps/bootcamp-all/bootcamp-all.component.ts
+++ src/app/features/components/bootcamps/bootcamp-all/bootcamp-all.component.ts
@@ -4,21 +4,29 @@ import { BootcampService } from '../../../services/concretes/bootcamp.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { DeadLinePipe } from '../../../../shared/pipes/dead-line.pipe';
+import { PageRequest } from '../../../../core/models/pagination/page-request';
+import { PaginatorModule } from 'primeng/paginator';
+import { BootcampListItemDto } from '../../../models/responses/bootcamps/bootcamp-list-item-dto';
 
 @Component({
   selector: 'app-bootcamp-all',
   standalone: true,
-  imports: [CommonModule, DeadLinePipe],
+  imports: [CommonModule, DeadLinePipe,PaginatorModule],
   templateUrl: './bootcamp-all.component.html',
   styleUrl: './bootcamp-all.component.scss'
 })
 export class BootcampAllComponent implements OnInit {
   allBootcamps!: GetBootcampResponse[];
+  allFinishedBootcamps!: GetBootcampResponse[];
+  allBootcampResponse!: BootcampListItemDto<GetBootcampResponse>;
+  bootcampCount!:number;
+  readonly PAGE_SIZE = 6;
 
   constructor(private bootcampService: BootcampService, private router: Router) { }
 
   ngOnInit(): void {
     this.getListUnfinished();
+    this.getListFinished({pageIndex:0,pageSize:this.PAGE_SIZE});
   }
 
   getListUnfinished() {
@@ -26,9 +34,22 @@ export class BootcampAllComponent implements OnInit {
       this.allBootcamps = response.items;
     })
   }
-
+  getListFinished(pageRequest: PageRequest) {
+    this.bootcampService.getListFinished(pageRequest).subscribe((response) => {
+      this.allBootcampResponse = response;   
+      this.allFinishedBootcamps=this.allBootcampResponse.items;
+      this.bootcampCount=response.count;
+    })
+  }
   navigateToBootcampDetail(bootcamp: GetBootcampResponse) {
     console.log("Selected bootcamp : ", bootcamp.id, bootcamp.name, bootcamp.instructorUserName)
     this.router.navigate(['/p', bootcamp.id])
+  }
+  onPageChange(event:any){
+    const pageRequest: PageRequest = {
+      pageIndex: event.page,
+      pageSize: this.PAGE_SIZE
+    };
+    this.getListFinished(pageRequest);
   }
 }
